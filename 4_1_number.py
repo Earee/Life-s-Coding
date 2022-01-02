04a6dcb5ff8c5af6b9489cae54e584c65499471b
Earee <96780715+Earee@users.noreply.github.com>
2022-01-02 21:13:18 +0900

    Add files via upload

---
+++ 4_1_number.py
@@ -0,0 +1,12 @@
+print(-1)
+print (0) #int
+print(1.1) #float
+print(1+1)
+print( '1+1', 1+1)
+
+import math
+print(math.sqrt(4))
+
+import random
+print('random.random()',random.random())
+
---
+++ 4_2_text.py
@@ -0,0 +1,11 @@
+print('hello world')
+print("hello world")
+print('''
+ii
+eadff
+rr
+''')
+
+print("'1'+'1'",'1'+'1')
+print('hello'*12)
+print(len('hello'*12))
